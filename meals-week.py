159e48a31dd9f0c48dcae30bdfed3877ce219371
Pablo Hendrick <116096331+pablohendrick@users.noreply.github.com>
2023-07-11 11:34:47 -0300

    Update meals-week.py

--- meals-week.py
+++ meals-week.py
@@ -1,19 +1,13 @@
-import random
+import random as rd
 
-# Set your favorite meals
 meals = ['your meal', 'your meal', 'your meal']
 
-# Set days of the week
 days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-# Create weekly table
 table_meals = {}
 for day in days_of_the_week:
-    # Select random meal
     aleatory_meals = random.choice(meals)
-    # Add meals in the table
     table_meals[day] = aleatory_meal
 
-# Print table
 for day, meal in table_meals.items():
     print(day + ': ' + meal)
